f4eea3d816264bc83e6f3eb8131dd93c36e6442e
NikSin <nikitasin222@gmail.com>
2017-12-18 15:49:20 +0200

    hw1

---
+++ module-1/js/test-js.js
@@ -0,0 +1,22 @@
+const upper = "qwertyuiop[]";
+const average = "asdfghjkl;'\\";
+const bottom = "zxcvbnm,./";
+
+
+console.log(upper);
+console.log(average);
+console.log(bottom);
+
+console.log(upper.length);
+console.log(average.length);
+console.log(bottom.length);
+
+console.log(upper.charAt(0));
+console.log(average.charAt(0));
+console.log(bottom.charAt(0));
+
+console.log(upper.indexOf('[]'));
+console.log(average.indexOf('[]'));
+console.log(bottom.indexOf('[]'));
+
+console.log(upper,average,bottom, upper.charAt(0), upper.charAt(upper.length-1), average.charAt(0), average.charAt(average.length-1), bottom.charAt(0), bottom.charAt(bottom.length-1), upper.indexOf('['))
